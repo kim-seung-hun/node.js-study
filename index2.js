f0639edf6f57a5c489674a8d541c4a27d46cac80
kim-seung-hun <rlatmdgns16@naver.com>
2022-07-21 16:45:33 +0900

    node.js(3) 20220721

--- index2.js
+++ index2.js
@@ -5,7 +5,7 @@
 // npm init -y  >>  json 파일 생성됨
 // y는 yes의 줄임말
 
-//터미널 취소 명령어 <컨트롤 + c>
+// 터미널 취소 명령어 <컨트롤 + c>
 
 // {
 //   "name": "20220718", >> name pakage의 이름
@@ -20,26 +20,26 @@
 //   "license": "ISC"
 // }
 
-//name
-//프로젝트의 이름이다.
+// name
+// 프로젝트의 이름이다.
 
-//version
-//프로젝트의 version을 정의 한다.
+// version
+// 프로젝트의 version을 정의 한다.
 
-//description
-//프로젝트에 대한 설명 , 문자열로 작성 가능하다.
+// description
+// 프로젝트에 대한 설명 , 문자열로 작성 가능하다.
 
-//keywords
-//프로젝트 검색할때 참조되는 키워드
+// keywords
+// 프로젝트 검색할때 참조되는 키워드
 
-//author
-//프로젝트 작업자 정보
+// author
+// 프로젝트 작업자 정보
 
-//scripts
-//프로젝트에서 자주 실행해야 하는 명령어를 script로 작성해두면 npm 명령어로 편하게 실행 가능하다.
-//"scripts" : {"start" : "node index.js" , "test" : "node index2.js"}
-//이렇게 작성하고 실행은 npm start
-//start가 아니면 실행 명령어는 run을 붙여서 npm run test
+// scripts
+// 프로젝트에서 자주 실행해야 하는 명령어를 script로 작성해두면 npm 명령어로 편하게 실행 가능하다.
+// "scripts" : {"start" : "node index.js" , "test" : "node index2.js"}
+// 이렇게 작성하고 실행은 npm start
+// start가 아니면 실행 명령어는 run을 붙여서 npm run test
 
-//license
-//모듈의 라이센스를 기록하는데 사용
+// license
+// 모듈의 라이센스를 기록하는데 사용
--- src/main.js
+++ src/main.js
@@ -29,9 +29,11 @@ const temp = mysql.createConnection({
 // database : 'test3' = test3 이름의 데이터 베이스를 사용하겠음
 // query함수의 첫번째 매개변수는 쿼리문을 입력해주고
 // 두번째 매개변수는 콜백 함수 매개변수는 첫번째 쿼리 에러, 두번째 쿼리 결과 이후 등등
-temp.query("SELECT * FROM posts", (err, res) => {
+temp.query("SELECT * FROM products", (err, res) => {
   if (err) {
-    console.log("안됨");
+    const sql =
+      "CREATE TABLE products(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), number VARCHAR(20), series VARCHAR(20))";
+    temp.query(sql);
   } else {
     console.log(res);
   }
@@ -41,10 +43,54 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   req.statusCode = 200;
+  // 한글이 깨지면 ... 인코딩 방식을 정해보자
+  // res.setHeader(내용) 함수를 사용해서 헤더의 정보를 설정 할 수 있다.
+  // utf-8로 컨텐츠 내용을 인코딩하는 속성을 추가한다하면,
+  res.setHeader("Content-Type", "application/json", "charset=utf-8");
 
-  res.write("123");
+  // 요청된 url 확인
+  // req.url
 
-  res.end("456");
+  // 요청된 method 확인
+  // req.method
+
+  // js 내용이 수정되었을때 자동으로 모니터링 해서 서버를 재시작 해주는 퉅
+  // nodemon : 노드 모니터링
+  // nodemon 설치 명령어
+  // ----------------------------------------------------
+  // npm install --save-dev nodemon
+
+  // 터미널 창에서 직접
+  // npm install -g nodemon
+  // ----------------------------------------------------
+  const URL = req.url;
+
+  switch (URL) {
+    case "/":
+      res.end("메인 페이지");
+      break;
+    case "/list":
+      temp.query("SELECT * FROM products", (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          // data에는 products 테이블 안의 컬럼 내용
+          res.end(JSON.stringify(data));
+        }
+      });
+      break;
+    case "/add":
+      // (name, number, series)VALUES(?,?,?) 이렇게 밸유의 값을
+      // 두번째 배열 타입의 매개변수로 추가할수 있다.
+      // eslint-disable-next-line no-case-declarations
+      const sql = "INSERT INTO products (name, number, series)VALUES(?,?,?)";
+      temp.query(sql, ["이름", "123", "123"]);
+      break;
+    default:
+      break;
+  }
+
+  console.log(req.url);
 });
 
 const PORT = 3000;
